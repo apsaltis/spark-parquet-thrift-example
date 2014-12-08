4f104e5910bdd022ecc96707e1b391f53eb35e14
theclaymethod <clayton.kim@gmail.com>
2014-12-08 13:42:40 -0800

    Spark Avro Parquet Examples

---
+++ src/main/scala/SparkParquetAvroApp.scala
@@ -0,0 +1,117 @@
+package avrotest
+
+import com.google.common.io.Files
+
+import org.apache.hadoop.fs.Path
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.catalyst.util.getTempFilePath
+
+// our own classes generated from user.avdl by Avro tools
+import avrotest.avro.{Message, User}
+import avrotest.UserOperations._
+
+/*
+sbt assembly && ~SPARK_HOME/bin/spark-submit \
+  --class "avrotest.ParquetAvroSparkExample" \
+  --master "local[*]" \
+  target/scala-2.10/SparkParquetThrift.jar
+ */
+
+object ParquetAvroSparkExample {
+
+  private var sqc: SQLContext = _
+
+  def main(args: Array[String]) {
+    // Create a Spark Configuration for local cluster mode
+    val conf = new SparkConf(true)
+      .setMaster("local")
+      .setAppName("ParquetAvroExample")
+
+    // Create a Spark Context and wrap it inside a SQLContext
+    sqc = new SQLContext(new SparkContext(conf))
+
+    // Prepare some input data
+    val avroFiles = (getTempFilePath("users", ".avro"), getTempFilePath("messages", ".avro"))
+    val parquetFiles = (
+      new Path(Files.createTempDir().toString, "users.parquet"),
+      new Path(Files.createTempDir().toString, "messages.parquet"))
+
+    // Generate some input (100 users, 1000 messages) and write then as Avro files to the local
+    // file system
+    writeAvroFile(avroFiles._1, createUser, 100)
+    writeAvroFile(avroFiles._2, createMessage(100)_, 1000)
+    // Now convert the Avro file to a Parquet file (we could have generated one right away)
+    convertAvroToParquetAvroFile(
+      new Path(avroFiles._1.toString),
+      new Path(parquetFiles._1.toString),
+      User.getClassSchema,
+      sqc.sparkContext.hadoopConfiguration)
+    convertAvroToParquetAvroFile(
+      new Path(avroFiles._2.toString),
+      new Path(parquetFiles._2.toString),
+      Message.getClassSchema,
+      sqc.sparkContext.hadoopConfiguration)
+
+    // Import the Parquet files we just generated and register them as tables
+    sqc.parquetFile(parquetFiles._1.getParent.toString)
+      .registerTempTable("UserTable")
+    sqc.parquetFile(parquetFiles._2.getParent.toString)
+      .registerTempTable("MessageTable")
+
+    // Now let's do some queries
+    println("The age of User3:")
+    println(findAgeOfUser("User3", sqc))
+    println("The favorite color of User4:")
+    println(findFavoriteColorOfUser("User4", sqc))
+    println("Favorite color distribution:")
+    val result = findColorDistribution(sqc)
+    for (color <- result.keys) {
+      println(s"color: $color count: ${result.apply(color)}")
+    }
+    findNumberOfMessagesSent(sqc).foreach {
+      case (sender, messages) => println(s"$sender sent $messages messages")
+    }
+    findMutualMessageExchanges(sqc).foreach {
+      case (user_a, user_b) => println(s"$user_a and $user_b mutually exchanged messages")
+    }
+    println("Count words in messages:")
+    countWordsInMessages(sqc).toTraversable.foreach {
+      case (word, count) => println(s"word: $word count: $count")
+    }
+
+    // Stop the SparkContext
+    sqc.sparkContext.stop()
+
+    // What follows is an example of how to use Avro objects inside Spark directly. For that we
+    // need to register a few Kryo serializers. Note: this is only required if we would like to
+    // use User objects inside Spark's MapReduce operations. For Spark SQL this is not required
+    // and in fact it seems to mess up the Parquet Row serialization(?).
+    setKryoProperties(conf)
+    val sc = new SparkContext(conf)
+
+    def myMapFunc(user: User): String = user.toString
+
+    println("Let's load the User file as a RDD[User], call toString() on each and collect the result")
+    val userRDD: RDD[User] = readParquetRDD[User](sc, parquetFiles._1.toString)
+    userRDD.map(myMapFunc).collect().foreach(println(_))
+    sc.stop()
+  }
+
+  /**
+   * Note1: Spark uses Kryo for serializing and deserializing Objects contained in RDD's
+   * and processed by its functional operators. In order to use Avro objects as part
+   * of those operations we need to register them and specify and appropriate (De)Serializer.
+   * Note2: This step is not neccesary if we one rely on relation operations of Spark SQL,
+   * since these use Row objects that are always serializable.
+   */
+  def setKryoProperties(conf: SparkConf) {
+    conf.set("spark.kryo.registrator", classOf[SparkAvroKryoRegistrator].getName)
+    conf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+    conf.set("spark.kryoserializer.buffer.mb", 4.toString)
+    conf.set("spark.kryo.referenceTracking", "false")
+  }
+}
---
+++ src/main/scala/UserOperations.scala
@@ -0,0 +1,259 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package avrotest
+
+import java.io.File
+
+import scala.util.Random
+
+import parquet.avro.{AvroReadSupport, AvroParquetWriter}
+import parquet.hadoop.ParquetInputFormat
+
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.Path
+import org.apache.hadoop.mapred.JobConf
+
+import org.apache.avro.Schema
+import org.apache.avro.file.{DataFileReader, DataFileWriter}
+import org.apache.avro.generic.{GenericDatumReader, IndexedRecord}
+import org.apache.avro.mapred.FsInput
+import org.apache.avro.specific.{SpecificRecord, SpecificDatumWriter}
+
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SQLContext
+import scala.reflect.ClassTag
+
+// our own class generated from user.avdl by Avro tools
+import avrotest.avro.{Message, User}
+
+// Implicits
+import org.apache.spark.SparkContext._
+import collection.JavaConversions._
+
+object UserOperations {
+
+  var random: Random = new Random(15485863l)
+
+  /**
+   * For the given username, find the favorite number of the user.
+   *
+   * @param name User name
+   * @param sqc The SQLContext to use
+   * @return The user's age
+   */
+  def findAgeOfUser(name: String, sqc: SQLContext): Int = {
+    sqc.sql("SELECT age FROM UserTable WHERE name = \"" + name + "\"")
+      .collect()
+      .apply(0)
+      .getInt(0)
+  }
+
+  /**
+   * For the given username, find the favorite color of the user.
+   *
+   * @param name User name
+   * @param sqc The SQLContext to use
+   * @return The favorite color
+   */
+  def findFavoriteColorOfUser(name: String, sqc: SQLContext): String = {
+    sqc.sql("SELECT favorite_color FROM UserTable WHERE name = \"" + name + "\"")
+      .collect()
+      .apply(0)
+      .getString(0)
+  }
+
+  /**
+   * For every color that is contained in the table, find the number of
+   * users whose favorite color matches that color.
+   *
+   * @param sqc The SQLContext to use
+   * @return A map of colors to the number of its occurrences
+   */
+  def findColorDistribution(sqc: SQLContext): Map[String, Long] = {
+    val result = new collection.mutable.HashMap[String, Long]()
+    val colorCounts = sqc.sql("SELECT favorite_color, COUNT(name) FROM UserTable GROUP BY favorite_color")
+      .collect()
+    for(row <- colorCounts) {
+      result += row.getString(0) -> row.getLong(1)
+    }
+    result.toMap
+  }
+
+  /**
+   * For each user in the UserTable find the number of messages sent in the
+   * MessageTable.
+   *
+   * @param sqc The SQLContext to use
+   * @return A list of pairs (user name, number of messages sent by that user)
+   */
+  def findNumberOfMessagesSent(sqc: SQLContext): Seq[(String, Long)] = {
+    sqc.sql("""
+        SELECT name, COUNT(recipient) FROM
+          UserTable JOIN MessageTable ON UserTable.name = MessageTable.sender
+            GROUP BY name ORDER BY name""")
+      .collect()
+      .map(row => (row.getString(0), row.getLong(1)))
+  }
+
+  /**
+   * Find all pairs of users from the MessageTable that have mutually exchanged
+   * messages. Note: this may report duplicate pairs (User1, User2) and (User2, User1).
+   *
+   * @param sqc The SQLContext to use
+   * @return A list of pairs (user name, user name)
+   */
+  def findMutualMessageExchanges(sqc: SQLContext): Seq[(String, String)] = {
+    sqc.sql("""
+        SELECT DISTINCT A.sender, B.sender FROM
+          (SELECT sender, recipient FROM MessageTable) A
+        JOIN
+          (SELECT sender, recipient FROM MessageTable) B
+        ON A.recipient = B.sender AND A.sender = B.recipient""")
+      .collect()
+      .map(row => (row.getString(0), row.getString(1)))
+  }
+
+  /**
+   * Counter the number of occurrences of each word contained in a message in
+   * the MessageTable and returns the result as a word->count Map.
+   *
+   * @param sqc he SQLContext to use
+   * @return A Map that has the words as key and the count as value
+   */
+  def countWordsInMessages(sqc: SQLContext): Map[String, Int] = {
+    sqc.sql("SELECT content from MessageTable")
+      .flatMap(row =>
+      row.getString(0).replace(",", "").split(" "))
+      .map(word => (word, 1))
+      .reduceByKey(_ + _)
+      .collect()
+      .toMap
+  }
+
+  /**
+   * Read in a parquet file containing Avro data and return the result as an RDD.
+   *
+   * @param sc The SparkContext to use
+   * @param parquetFile The Parquet input file assumed to contain Avro objects
+   * @return An RDD that contains the data of the file
+   */
+  def readParquetRDD[T <% SpecificRecord](sc: SparkContext, parquetFile: String)(implicit tag: ClassTag[T]): RDD[T] = {
+    val jobConf= new JobConf(sc.hadoopConfiguration)
+    ParquetInputFormat.setReadSupportClass(jobConf, classOf[AvroReadSupport[T]])
+    sc.newAPIHadoopFile(
+      parquetFile,
+      classOf[ParquetInputFormat[T]],
+      classOf[Void],
+      tag.runtimeClass.asInstanceOf[Class[T]],
+      jobConf)
+      .map(_._2.asInstanceOf[T])
+  }
+
+  /**
+   * Generates a of Avro objects and stores them inside an Avro file.
+   *
+   * @param file The output file
+   * @param factoryMethod The function to call to actually create the objects
+   * @param count The number of objects to generate
+   */
+  def writeAvroFile[T <: SpecificRecord](
+                                          file: File,
+                                          factoryMethod: Int => T,
+                                          count: Int): Unit = {
+    val prototype = factoryMethod(0)
+    val datumWriter = new SpecificDatumWriter[T](
+      prototype.getClass.asInstanceOf[java.lang.Class[T]])
+    val dataFileWriter = new DataFileWriter[T](datumWriter)
+
+    dataFileWriter.create(prototype.getSchema, file)
+    for(i <- 1 to count) {
+      dataFileWriter.append(factoryMethod(i))
+    }
+    dataFileWriter.close()
+  }
+
+  /**
+   * Converts an Avro file that contains a set of Avro objects to a Parquet file.
+   *
+   * @param input The Avro file to convert
+   * @param output The output file (possibly on HDFS)
+   * @param schema The Avro schema of the input file
+   * @param conf A Hadoop `Configuration` to use
+   */
+  def convertAvroToParquetAvroFile(
+                                    input: Path,
+                                    output: Path,
+                                    schema: Schema,
+                                    conf: Configuration): Unit = {
+    val fsInput = new FsInput(input, conf)
+    val reader =  new GenericDatumReader[IndexedRecord](schema)
+    val dataFileReader = DataFileReader.openReader(fsInput, reader)
+    val parquetWriter = new AvroParquetWriter[IndexedRecord](output, schema)
+
+    while(dataFileReader.hasNext)  {
+      // Mote: the writer does not copy the passed object and buffers
+      // writers. Therefore we need to pass a new User object every time,
+      // although the reader allows us to re-use the same object.
+      parquetWriter.write(dataFileReader.next())
+    }
+
+    dataFileReader.close()
+    parquetWriter.close()
+  }
+
+  /**
+   * Creates a User Avro object.
+   *
+   * @param id The ID of the user to generate
+   * @return An Avro object that represents the user
+   */
+  def createUser(id: Int): User = {
+    val builder = User.newBuilder()
+      .setName(s"User$id")
+      .setAge(id / 10)
+    if (id >= 5) {
+      builder
+        .setFavoriteColor("blue")
+        .build()
+    } else {
+      builder
+        .setFavoriteColor("red")
+        .build()
+    }
+  }
+
+  /**
+   * Creates a Message Avro object.
+   *
+   * @param id The ID of the message to generate
+   * @return an Avro object that represents the mssage
+   */
+  def createMessage(maxUserId: Int)(id: Int): Message = {
+    val sender = random.nextInt(maxUserId)
+    var recipient = random.nextInt(maxUserId)
+    while (recipient == sender) recipient = random.nextInt(maxUserId)
+
+    Message.newBuilder()
+      .setID(id)
+      .setSender(s"User$sender")
+      .setRecipient(s"User$recipient")
+      .setContent(s"Hey there, User$recipient, this is me, User$sender")
+      .build()
+  }
+}
